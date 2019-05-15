a51394e4703ea91808ddcbd3e84b33bf14f248d2
Ringmaster <1164366628@qq.com>
2019-05-15 11:25:46 +0800

    Update template.js

--- javascript/template.js
+++ javascript/template.js
@@ -2,18 +2,13 @@ function getData(tpl){
     var data = {};
     data["title"] = 'fasdfasf';
     data["color"] = 'red';
-    data["list"] = [
-        {title: '标题1', desc: '描述1'},
-        {title: '标题2', desc: '描述2'},
-        {title: '标题3', desc: '描述3'},
-    ]
     var scriptResult = comilerSpecialElement(tpl,"script");
     tpl = scriptResult.tpl;
     var styleResult = comilerSpecialElement(tpl,"style");    
     tpl = revertSpecialElement(tpl,"script",scriptResult.result);
     tpl = revertSpecialElement(tpl,"style",styleResult.result);
-    getScriptFragment(tpl,'if',data);
-    //document.body.innerHTML = revertContent(tpl,data);
+    tpl = getScriptFragment(tpl,data);
+    document.body.innerHTML = revertContent(tpl,data)
 }
 getData(document.body.innerHTML);
 /**
@@ -45,28 +40,33 @@ function revertSpecialElement(tpl,tagname,revertObj){
     }
     return tpl;
 }
-
-function getScriptFragment(tpl,keyword,data){
-    var reg = new RegExp("{{\\s*"+keyword+"\\s*(\\S+)\\s*}}([\\s\\S]*?){{\\s*"+keyword+"end\\s*}}");
-    console.log(reg)
-    var result= reg.exec(tpl);
-    if(data[result[1].trim()]){
-        console.log(1111)
+/**
+ * @description 获取逻辑片段并且替换
+ * @param {DocumentFragment} tpl
+ * @param {Ojbect} data
+ * @returns {DocumentFragment} 
+ */
+function getScriptFragment(tpl,data){
+    var reg =/{{\s*\b([\w]*?)\b\s+([\s\S]*?\s*)}}([\s\S]*?){{/g;
+    var reg2 =/{{\s*\b([\w]*?)\b\s+([\s\S]*?\s*)}}([\s\S]*?){{\s*(\/[\w]*?)\s+}}/g;
+    var result = null;
+    var code = '';
+    var fragment = reg2.exec(tpl)[0];
+    while(result = reg.exec(tpl)){
+        if(data[result[2].trim()] || eval(result[2])){
+            code = result[3];
+        }
     }
-}
-
-function revertKeyword(tpl){
-    var regIf = /{{\s*(if)\s*(\S+)\s*}}/;
-    var regEIf = /{{\s*(\S+)\s*(\S+)\s*(\S+)\s*}}/
-    var regElse = /{{\s*(\S+)\s*}}/;
-    if(regIf.test(tpl)){
-        console.log(regIf.exec(tpl))
+    if(!code){
+        var reg1 =/{{\s*\b([\w]*?)\b\s+}}([\s\S]*?){{/g;
+        result = reg1.exec(tpl);
+        if(result){
+            code = result[2]
+        }
     }
-    // while(regIf.test(tpl)){
-    //    
-    // }
+    return tpl.replace(fragment,code)
+    
 }
-
 
 /**
  * 
