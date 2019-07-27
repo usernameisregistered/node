bc0eb0c0e379c204fab321e78e5970a9b41d1b34
usernameregistered <1164366628@qq.com>
2019-07-27 15:53:36 +0800

    图片压缩功能封装

--- compressImage.js
+++ compressImage.js
@@ -1,3 +1,4 @@
+
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(factory);
@@ -8,52 +9,62 @@
     }
 }(this, function () {
     /**
-     * @description 图片压缩 一 根据指定的图片大小压缩（不改变图像的宽度修改文件的清晰度） 二 根据给定的宽度或者高度压缩 三 根据指定的缩放比例进行等比缩放
-     * @param {File} file 
-     * @param {Object} config 
+     * @description 图片压缩 
+     * 一 根据指定的大小对图片压缩（不改变图像的宽度修改文件的清晰度） canvas目前仅支持对后缀为jpeg的图片进行压缩
+     * 二 根据给定的宽度或者高度裁剪 
+     * 三 根据指定的缩放比例进行裁剪 
+     * 四 根据指定的缩放比例进行缩放 图片大小可能会变大 比较图片的分辨率
+     * 
+     * @params {File} file 
+     * @params {Object} config 
+     * @author liming
+     * @date 2019-7-27
+     * @version 1.0.0
      */
     /*
         config={
             unitsList:['bit', 'kb', 'mb', 'gb'] 允许的文件大小的单位
             units: 'bit', 默认的文件大小单位
-            suffixList:['image/png','image/webp'], 允许的图片的格式 不建议修改 其他的图片的格式默认按照image/png 
-            suffix:'image/png', 默认的图片格式 不建议修改
-            type: 1|2|3 图片的压缩方式 默认是一
+            suffixList:['image/png','image/webp','image/jpeg'],
+                允许转换的图片的格式 不建议修改 Chrome支持“image/webp”类型 压缩后的图片统一后缀为jpeg png格式不支持清晰度压缩
+            suffix:'image/jpeg', 默认的图片格式 不建议修改
+            type: 1|2|3|4 图片的压缩方式 默认是一
             ohterConfig:{
-                width:'', 压缩的图片宽度 类型为2 参数有效
-                height:'',压缩的图片高度 类型为2 参数有效
-                zoomFactor: number 缩放比例 类型为3 参数有效
+                width:'', 压缩后的图片宽度 类型为2 参数有效
+                height:'',压缩后图片高度 类型为2 参数有效
+                offset:['xoffset','yoffset'] 先裁剪后进行其他的图片裁剪的偏移值 类型为2,3 参数有效
+                zoomFactor: [zfactor,yfactor] 缩放比例 类型为3,4 参数有效
                 compressratee：number > 0 && < 1 压缩比 类型为1 参数有效 
                 size:number|string 要压缩的最大字节数 类型为1 参数有效 
             }
-
         }
     */
     function compressImage(file, config) {
         this.config = {
             unitsList: ['bit', 'kb', 'mb', 'gb'],
             units: 'bit',
-            suffixList:['image/png','image/webp'],
-            suffix:'image/png',
-            type:1,
-            ohterConfig:{
-                width:'',
-                height:'',
-                zoomFactor:0.8,
-                compressratee:0.92,
-                size:'2kb'
+            suffixList: ['image/png', 'image/webp','image/jpeg'],
+            suffix: 'image/jpeg', 
+            type: 4,
+            ohterConfig: {
+                width: '400',
+                height: '400',
+                zoomFactor: [0.4,0.4],
+                compressratee: 0.92,
+                offset:[100,100],
+                size: '200kb'
             }
         }
-        for(let key in config){
-           if(key != 'ohterConfig'){
-            this.config[key] =  config[key]
-           }else{
-               for(let item in config['ohterConfig']){
-                this.config['ohterConfig'][item] = config['ohterConfig'][item]
-               }
-           }
+        for (let key in config) {
+            if (key != 'ohterConfig') {
+                this.config[key] = config[key]
+            } else {
+                for (let item in config['ohterConfig']) {
+                    this.config['ohterConfig'][item] = config['ohterConfig'][item]
+                }
+            }
         }
-        if(this.config.type == 1){
+        if (this.config.type == 1) {
             this.convSize();
         }
         this.file = file;
@@ -65,56 +76,68 @@
         this.getImageInfo();
         this.blob = '';
     };
-    compressImage.prototype = {      
-        getImageInfo(){
-            this.reader.addEventListener("load",()=>{
+    compressImage.prototype = {
+        getImageInfo() {
+            this.reader.addEventListener("load", () => {
                 this.img.src = this.reader.result;
                 this.imageData = this.reader.result;
-                this.img.addEventListener('load',()=>{
-                    if(this.config.type == 2 ){
-                        this.config.ohterConfig.width = this.config.ohterConfig.width;
-                        this.config.ohterConfig.height = this.config.ohterConfig.height;
-                    }else if(this.config.type == 3){
-                        if(this.config.ohterConfig.zoomFactor){
-                            this.config.ohterConfig.width *= this.config.ohterConfig.zoomFactor;
-                            this.config.ohterConfig.height *= this.config.ohterConfig.zoomFactor;
-                        }else{
-                            throw new Error("缺少必要的参数缩放比例：zoomFactor")
-                        }
-                    } else{
-                        this.config.ohterConfig.width = this.img.width;
-                        this.config.ohterConfig.height = this.img.height;
+                this.file.size = this.imageData.length ;
+                this.img.addEventListener('load', () => {
+                    switch(this.config.type + ''){
+                        case "1":
+                            this.canvas.width = this.config.ohterConfig.width = this.img.naturalWidth;
+                            this.canvas.height= this.config.ohterConfig.height = this.img.naturalHeight;
+                            break;
+                        case "2":
+                            this.canvas.width = this.config.ohterConfig.width; 
+                            this.canvas.height = this.config.ohterConfig.height;
+                            break; 
+                        case "3":
+                            this.canvas.width = this.config.ohterConfig.width  = (this.img.naturalWidth - this.config.ohterConfig.offset[0] ) * this.config.ohterConfig.zoomFactor[0]; 
+                            this.canvas.height = this.config.ohterConfig.height = (this.img.naturalHeight - this.config.ohterConfig.offset[1] ) * this.config.ohterConfig.zoomFactor[1];
+                            break; 
+                        case "4":
+                            this.canvas.width = this.config.ohterConfig.width  = this.img.naturalWidth * this.config.ohterConfig.zoomFactor[0];
+                            this.canvas.height = this.config.ohterConfig.height = this.img.naturalHeight * this.config.ohterConfig.zoomFactor[1];
+
                     }
-                    this.canvas.width = this.img.width;
-                    this.canvas.height =  this.img.height;
                     this.drawImage();
-                    if(this.config.type == 1 ){
-                        while(this.config.ohterConfig.size * 2 < this.imageData.length){
-                            let tempimg = new Image();
-                            tempimg.src = this.imageData;
-                            img.addEventListener('load',()=>{
-                                this.context.drawImage(tempimg,0,0,this.config.ohterConfig.width,this.config.ohterConfig.height);
-                                this.imageData = this.canvas.toDataURL(this.config.suffix,this.config.ohterConfig.compressratee); 
-                            },{once:true})
-                            
-                        }
-                    }
-                },{once:true})
-            }, false);
-            
-            /* 当imageData为undefined时 读取上传的文件否则读取压缩后base64 */
-            if(!this.imageData){
-                this.reader.readAsDataURL(this.file)
-            }
-            
+                }, {
+                    once: true
+                })
+            },  {
+                once: true
+            });
+            this.reader.readAsDataURL(this.file)
         },
         /**绘制图片*/
-        drawImage(){
-            this.context.drawImage(this.img,0,0,this.config.ohterConfig.width,this.config.ohterConfig.height);
-            this.imageData = this.canvas.toDataURL(this.config.suffix,this.config.ohterConfig.compressratee);            
+        drawImage() {  
+            this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+            switch (this.config.type + '') {
+                case '1':
+                    this.context.drawImage(this.img, 0, 0, this.config.ohterConfig.width, this.config.ohterConfig.height);
+                    this.config.ohterConfig.scompressratee = this.file.size > this.config.ohterConfig.size ?this.config.ohterConfig.size / this.file.size : 1;
+                    this.imageData = this.canvas.toDataURL(this.config.suffix,this.config.ohterConfig.scompressratee * 1);
+                    if(this.config.suffix == "image/png"){
+                        console.warn("png格式的图片目前不支持清晰度压缩")
+                    }
+                    break;
+                case "2":
+                    this.context.drawImage(this.img,this.config.ohterConfig.offset[0],this.config.ohterConfig.offset[1], this.config.ohterConfig.width, this.config.ohterConfig.height,0,0,this.canvas.width,this.canvas.height);
+                    this.imageData = this.canvas.toDataURL(this.config.suffix,1);
+                    break;
+                case '3':
+                    this.context.drawImage(this.img, this.config.ohterConfig.offset[0],this.config.ohterConfig.offset[1], this.config.ohterConfig.width, this.config.ohterConfig.height,0,0,this.canvas.width,this.canvas.height);
+                    this.imageData = this.canvas.toDataURL(this.config.suffix,1);
+                    break;
+                case "4":
+                    this.context.drawImage(this.img, 0,0, this.img.naturalWidth, this.img.naturalHeight,0,0,this.canvas.width,this.canvas.height);
+                    this.imageData = this.canvas.toDataURL(this.config.suffix,1);
+                    break;
+            }           
         },
 
-          /**
+        /**
          * 进制转换将单位全部转化为字节
          */
         convSize() {
@@ -135,8 +158,8 @@
                         this.config.ohterConfig.size = size * 1024 * 1024;
                         break;
                     case 'gb':
-                        this.config.ohterConfig.size = size  * 1024 * 1024 * 1024;
-                        break; 
+                        this.config.ohterConfig.size = size * 1024 * 1024 * 1024;
+                        break;
                 }
             } else {
                 throw new Error("转换单位不被允许")
